11174bf13576e811483731654e27b15d0a3b262a
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2023-04-21 17:19:22 +1200

    adjusted for types

--- src/routes/File.svelte
+++ src/routes/File.svelte
@@ -3,6 +3,7 @@
 	export let url: string;
 
 	let clicked = false;
+	let typesClicked = false;
 	let type = name.slice(name.lastIndexOf('.') + 1);
 </script>
 
@@ -46,6 +47,42 @@
 			<path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
 		</svg>
 	</button>
+	{#if type == 'gltf' || type == 'glb'}
+		<p>types</p>
+		<button
+			class="p-2"
+			on:click={() => {
+				typesClicked = true;
+				navigator.clipboard.writeText(window.location + url.replace(/\.(gltf|glb)/g, '.ts'));
+				setTimeout(() => (typesClicked = false), 500);
+			}}
+		>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+				class="w-6 h-6 {typesClicked ? 'hidden' : ''}"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244"
+				/>
+			</svg>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+				class="w-6 h-6 {!typesClicked ? 'hidden' : ''}"
+			>
+				<path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+			</svg>
+		</button>
+	{/if}
 </div>
 
 <style>
