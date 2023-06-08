f6418ad98ca117f1286fe5ba76371efb57a73f7d
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2023-06-09 09:42:27 +1200

    todo note

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
 	import '../app.css';
 	import Folder from './Folder.svelte';
 	import tree from './static-data.json';
+	// TODO-DefinitelyMaybe: Add link back to quaternius website
 </script>
 
 <Folder name="Static" contents={tree.contents} expanded />
