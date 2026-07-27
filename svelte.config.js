34b9c2199905c802e8898617b67c437cb58a021c
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2026-07-27 12:51:20 +1200

    fix: update CSRF trusted origins to allow all

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,7 @@ const config = {
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter(),
 		csrf: {
-			trustedOrigins: ['https://threlte.xyz']
+			trustedOrigins: ['*']
 		}
 	},
 
