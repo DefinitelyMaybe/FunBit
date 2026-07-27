e3af5473c3d1d537b47fcff1607ece1c61137e8e
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2026-07-27 13:16:10 +1200

    feat: add GitHub Actions workflow for build and deployment to GitHub Pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { mdsvex } from 'mdsvex';
-import adapter from '@deno/svelte-adapter';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -12,9 +12,12 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter(),
-		csrf: {
-			trustedOrigins: ['*']
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build'
+		}),
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/your-repo-name' : ''
 		}
 	},
 
